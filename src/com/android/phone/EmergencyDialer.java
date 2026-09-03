LineageOS/android_packages_services_Telephony

[BACKPORT] Prevent overlays on Telephony settings screen.
Ensure only system overlays are allowed on telephony settings screens.

Flag: NONE Security fix
Bug: 335387175
Test: Manually launched activities to verify no overlays are possible.
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:46e18ac57da3ac50271fc2be5b20bd7799637eae)
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ead85643a19379e0ff102a2bf5922c71acde603f)
Merged-In: I953667309753ff6fc966956e246512de97ad8638
Change-Id: I953667309753ff6fc966956e246512de97ad8638

src/com/android/phone/EmergencyDialer.java
@@ -336,6 +336,9 @@ public class EmergencyDialer extends Activity implements View.OnClickListener,
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
+        getWindow().addFlags(
+                WindowManager.LayoutParams.PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
+
         mMetricsWriter = new MetricsWriter();
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         if (mSensorManager != null) {

AICP-Revived/packages_services_Telephony

src/com/android/phone/EmergencyDialer.java
@@ -172,6 +172,9 @@ public class EmergencyDialer extends Activity implements View.OnClickListener,
 
         mStatusBarManager = (StatusBarManager) getSystemService(Context.STATUS_BAR_SERVICE);
 
+        getWindow().addPrivateFlags(
+                WindowManager.LayoutParams.PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
+
         // Allow this activity to be displayed in front of the keyguard / lockscreen.
         WindowManager.LayoutParams lp = getWindow().getAttributes();
         lp.flags |= WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;